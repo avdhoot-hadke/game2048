43cb080c2936f821ca36188e3bc847d582092b8c
avdhoot <avdhoothadke@gmail.com>
2025-10-14 22:19:34 +0530

    User Model and repo created

---
+++ backend/src/main/java/com/game2048/backend/model/User.java
@@ -0,0 +1,25 @@
+package com.game2048.backend.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int boardSize = 4;
+
+    private int highestScore = 0;
+}
